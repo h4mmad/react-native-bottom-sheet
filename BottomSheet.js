00e4e82f650060a52148c153c76bf498548d93a1
h4mmad <mohammedhammad07@gmail.com>
2023-03-01 18:36:43 +0300

    updated README.md

--- BottomSheet.js
+++ BottomSheet.js
@@ -15,9 +15,13 @@ import Animated, {
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
+// the minimum position of the bottom sheet, it won't move below that
 const MIN_TRANSLATE_Y = -SCREEN_HEIGHT / 4;
+
+// the maximum position of the bottom sheet, it won't move above that
 const MAX_TRANSLATE_Y = -SCREEN_HEIGHT + 100;
 
+// style your bottom sheet
 const styles = StyleSheet.create({
   bottomSheet: {
     width: "100%",
@@ -28,21 +32,37 @@ const styles = StyleSheet.create({
 });
 
 const BottomSheet = () => {
+  //translationY shared value from reanimated
   const translationY = useSharedValue(0);
+
+  //context, previous value from translationY
   const context = useSharedValue(0);
+
+  //optional
   const borderRadius = useSharedValue(25);
 
+  //Gesture.Pan() from react native gesture handler
   const gesture = Gesture.Pan()
+    //on gesture start store the translation.value in context.value
     .onStart(() => {
       context.value = translationY.value;
     })
+    //on update set the translationY value to event.translationY + context.value;
     .onUpdate((event) => {
+      //check to make sure the bottom sheet doesn't move below the MIN_TRANSLATE_Y value
       if (event.translationY + context.value < MIN_TRANSLATE_Y) {
         translationY.value = event.translationY + context.value;
         translationY.value = Math.max(MAX_TRANSLATE_Y, translationY.value);
       }
     })
+
+    //here will will check for the swipe threshold
+    //use event velocity to check if the swipe is up or down,
+
     .onEnd((event) => {
+      // when the swipe is down the event velocity is positive
+      // on pan gesture end check if the translationY.value is less than MAX_TRANSLATE_Y
+
       if (translationY.value > MAX_TRANSLATE_Y && event.velocityY > 0) {
         translationY.value = withSpring(MIN_TRANSLATE_Y, {
           damping: 30,
@@ -50,6 +70,8 @@ const BottomSheet = () => {
         borderRadius.value = withTiming(25);
       }
 
+      // when the swipe is up the event velocity is negative
+      // on pan gesture end check if the translationY.value is greater than MAX_TRANSLATE_Y
       if (translationY.value < MIN_TRANSLATE_Y && event.velocityY - 30 < 0) {
         translationY.value = withSpring(MAX_TRANSLATE_Y, { damping: 30 });
         borderRadius.value = withTiming(0);
--- README.md
+++ README.md
@@ -3,4 +3,11 @@
 - We will be using React Native Reanimated and React Native Gesture handler from Software Mansion.
 - React Native Reanimated performs animations on the native UI thread
 
+### Steps
+
+1. Import the Dimension's API to get the height of the window
+2. Make two positions, a maximum translateY and a minimum translateY
+3. Make your bottom sheet view.
+4. Use a pan gesture
+
 <img src="https://user-images.githubusercontent.com/55020862/222182943-2c405310-e153-4e34-a2dc-3db6d84c94ad.gif" width="200" height="100%">
